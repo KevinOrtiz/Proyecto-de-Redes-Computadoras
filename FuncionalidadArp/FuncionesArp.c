f0ead3bda8837ea522c282e32c764e0327724a6c
root <root@kevanort@espol.edu.ec>
2015-01-20 03:59:09 -0500

    Proyectos y ejemplos

---
+++ FuncionalidadArp/FuncionesArp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/int.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <netinet/if_ethet.h>
+///definimos las funcionaes que vamos a usar
+
---
+++ FuncionalidadArp/recursosdelibpap/lsdev.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/
