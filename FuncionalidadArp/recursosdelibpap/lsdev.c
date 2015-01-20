79d655be12a30eb7c9e8383783d4eaaaa82845a3
root <root@kevanort@espol.edu.ec>
2015-01-20 04:30:43 -0500

    Uso de deteccion de tarjeta de red como ejemplo

--- FuncionalidadArp/recursosdelibpap/lsdev.c
+++ FuncionalidadArp/recursosdelibpap/lsdev.c
@@ -3,4 +3,52 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/
+#include <arpa/inet.h>
+#include <pcap.h>
+
+///Este programa lo que hace es detectar los dispositivos de red connectados
+int main( int argc , char **argv)
+{
+  char *net;
+  char *mask;
+  char *dev;
+  int ret;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  bpf_u_int32 netp;
+  bpf_u_int32 maskp;
+  struct in_addr addr;
+  
+    if((dev=pcap_lookupdev(errbuf))==NULL) //se muestras las interfaces libres
+    {
+      printf("ERROR %s \n ",errbuf);
+      exit(-1);
+    }
+
+  //Se muestra el nombre del dispositivo
+    printf("NAME OF DEVISE : %s \n",dev);
+   
+  //Aqui queremos obtener la direccion de red y la mascara
+  if((ret=pcap_lookupnet(dev,&netp,&maskp,errbuf))==-1)
+   {
+    printf("ERROR %s \n ",errbuf);
+    exit(-1);
+    }
+   addr.s_addr=netp;
+   
+    if((net=inet_ntoa(addr))==NULL)
+    {
+    perror("inet_ntoa");
+    exit(-1);
+    }
+   
+    printf("Direccion de Red : %s \n",net);
+    addr.s_addr=maskp;
+    mask=inet_ntoa(addr);
+    if((net=inet_ntoa(addr))==NULL)
+    {
+    perror("inet_ntoa");
+    exit(-1);
+    }
+   printf("Mascara de Red %s \n",mask);
+ return 0;
+  }
