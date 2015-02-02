6725f3ec61b979f7f21271bf0a113d506756af65
root <root@kevanort@espol.edu.ec>
2015-02-02 01:00:11 -0500

    Terminado arpspoofin falta revision

--- FuncionalidadArp/FuncionesArp.c
+++ FuncionalidadArp/FuncionesArp.c
@@ -8,6 +8,7 @@
 #include <time.h>
 #include <netinet/if_ethet.h>
 #include <string.h>
+#define SIZE 100
 
 struct arpstruct{
 	u_char arp_mac[6];
@@ -56,7 +57,7 @@ void arpspoofin()
     pcap_t *handle;
     char errbufDispositivos[50];
     char devs[50];
-    int count=1;
+    int count=0;
     int number;
 
     struct arpstruct datos[50];
@@ -79,7 +80,7 @@ void arpspoofin()
      	    	   	 printf("NO SE ENCONTRO NINGUN DISPOSITIVO %s",errbuf);
      	    	   	 exit(1);
      	    	   }
-
+                
      	    	   for(dispositivos=dispositivosdisponibles;dispositivos !=NULL ;dispositivos=dispositivos->next)
      	    	   {
      	    	   	printf("%d->%s --->%s \n",count,dispositivos->name,dispositivos->description);
@@ -91,15 +92,346 @@ void arpspoofin()
 
      	    	   }
 
-     	    	   printf("NUMERO DE DISPOSITIVO PARA HACER SNIFFING \n");
+     	    	   printf("Numero de dispositivo para hacer Sniffing \n");
      	    	   scanf("%d",&number);
      	    	   printf("Tiempo de lapso maximo para hacer deteccion");
      	    	   scanf("%d",&countdown);
      	    	   dev=devs[number];
+                 //***********
+                   printf("\t Dispositivo :%s \t con un tiempo maximo %d seg /n",device,countdown);
+
+                  //Direccion de red y mascara obtenemos
+     	    	   pcap_lookupnet(dev,%netp,&maskp,errbuf);
      	    	   
+                   descriptor=pcap_open_live(dev,SIZE,1,15,errbuf);
+                   if(descriptor == NULL)
+                   {
+                   	 printf("pcap_open_live(): %s \n",errbuf );
+                   	 exit(1);
+                   }
+
+                   compiler=pcap_compile(desc,&fp,"arp",0,netp);
+                    
+                    if(compiler<0)
+                    {
+                    	printf("ERROR AL COMPILAR FILTRO \n");
+                    	exit(1);
+                    }
+ 
+                    filtro=pcap_setfilter(descriptor,&fp);
+                    
+                    if(filtro < 0)
+                    {
+                        fprintf(stderr, "error aplicando filtro\n");
+                    }
+                    struct pcap_pkthdr *header;
+                    const u_char *data;
+                    u_int contadorpackete=0;
+                    int valorretorno;
+                    int repetir=0;
+                     while(valorretorno=pcap_next_ex(descr,&header,&data) >= 0)
+                     {
+                        contadorpackete++;
+                     	if ( data != NULL)
+                     	{
+                     		const struct pcap_pkthdr* pkthdr=header;
+                     		count=contadorpackete;
+                     		packete=data;
+                     		u_int i;
+
+                              ///*****************************
+                            if(repetir != header->ts.tv_usec)
+                     	    {
+
+                              
+
+                                        printf("N.-Packet %i \n",contadorpackete);
+                                        printf("tamaño Paquete : %i \n",header->len); 
+
+                                 
+                                        //validaciones
+                                        if(header->len != header->caplen)
+                                        {
+                                        printf("SE CAPTURO TAMAÑOS DIFERENTE PARA ESE PACKETE : %i byte \n ",header->len);
+                                        printf("Tiempo transcurrido  %li %li segundos",header->ts.tv_sec,header->ts.tv_usec);
+                                    
+                                        }
+
+                                            int igualmac=0;
+                                            int igualip=0;
+                                            int spoofarp=0;
+                                            int v=0;
+                                            char resp=(char )2;
+
+
+
+
+                                  
+                                   for(i=0;(i <header->caplen);i++)
+                                   {
+                                     if(i==21 && (data[i]==resp))
+                                     {
+                                        if(j==0)
+                                        {
+                                       
+                                        for(n=0;n<6;n++)
+                                        {
+                                         datos[j].arp_mac[n]=data[22+n];
+                                        }
+
+                                        for (n=0 ; n<4 ;n++)
+                                        {
+                                          datos[j].arp_ip[n]=data[28+n];  
+                                        }
+                                         
+
+                                        }
+
+                                        else
+                                        {
+                                         int flagip=0;
+                                         int flagmac=0;
+                                         int px,py;
+                                         for(px=0;px < j;px++)
+                                         {
+                                            for(py=0;py<4;py++)
+                                            {
+                                                if(data[28+py]==datos[px].arp_ip[py])
+                                                {
+                                                    igualip++;
+                                                }
+                                            }
+                                         } 
+
+                                         if (igualip==4)
+                                         {
+                                           flagip=1;
+                                           flagmac=1;
+                                           break;
+                                         }       
+
+                                          //*************
+                                         igualip=0;
+
+
+
+                                        }
+
+                                        if (flagmac)
+                                        {
+                                            int mx,my;
+                                             for(my=0;my < 6;my++)
+                                             {
+                                                if(data[22+my] == datos[px].arp_mac[my])
+                                                 {
+                                                    igualmac++;
+                                                 }   
+                                             }
+
+                                             if(igualmac != 6)
+                                             {
+                                                spoofarp=1;
+                                                v=mx;
+
+                                             }
+                                             igualmac=0;
+                                             j--;
+                                        }
+
+                                        else
+                                        {
+                                            for(n=0;n<6;n++)
+                                            {
+                                                datos[j].arp_mac[n]=data[22+n];
+                                            }
+
+                                            for(n=0;n<4;n++)
+                                            {
+                                                datos[j].arp_ip[n]=data[28+n];
+                                            }
+
+                                        }
+
+                                     }
+
+                                     j++;
+                                   } 
+
+                                       if(i%16 == 0)
+                                        {
+                                        printf("\n");
+                                        printf("%.2x",data[i]);
+
+                                        }
+                                        if(spoofarp==1)
+                                        {
+                                        printf("\n\tDETECT: Who-has %i.%i.%i.%i, R1: %x:%x:%x:%x:%x:%x,  R2:  %x:%x:%x:%x:%x:%x, TS: %li . %li \n"
+                                        ,data[28],data[29],data[30],data[31]
+                                        ,datos[v].arp_mac[0], datos[v].arp_mac[1], datos.arp_mac[2], datos.arp_mac[3],datos.arp_mac[4], datos[v].arp_mac[5]
+                                        ,data[22],data[23],data[24],data[25],data[26], data[27]
+                                        ,header->ts.tv_sec, header->ts.tv_usec);
+                                        }
+
+                                        printf("\n\n");
+                                        repetir=header->ts.tv_usec; 
+                            }
+
+
+
+
+
+
+
+                              //****
+
+                           
+                     		  	      
+                        }
+                   }
      	    }
-     }
+
+        else if(strcmp(opciones,"-r")==0)
+        {
+            printf("ingrese el nombre del archivo \n ");
+            scanf("%s",nombreArchivo);
+
+            strcat(FILE,nombreArchivo);
+            strcat(FILE,extension);
+
+            pcap_t *pcap=pcap_open_offline(nombreArchivo,errbuf);
+            struct pcap_pkthdr *header;
+            const u_char *data;
 
 
+            u_int packetcount=0;
+            int  returnvalue;
+            int arpi=0;
+            int d=0;
+            char resp= (char )2;
+
+             while(returnvalue =pcap_next_ex(pcap,&header,$data)>=0)
+             {
+                  packetcount++;
+                  const struct pcap_pkthdr* pkthdr=header;
+                  int count=packetcount;
+                  const u_char* packet = data;
+                  u_int i;
+
+                  printf("Packete N.- %d",packetcount);
+                  printf("Longitud del packete %d",header->len);
+
+                  if(header->len != header->caplen)
+                  
+
+                    {
+                    printf("Capturo packetes de diferentes tamaño");
+                    printf("time %li:%li seg \n",header->ts.tv_sec,header->ts.tv_usec);
+                     }
+                    int coincidemac=0;
+                    int coincideip=0;
+                    int spoofarp=0;
+                    int v=0;
+
+                      for (i=0; (i <header->caplen) ; i++){
+                              if(i==21 && (data[i] == resp))
+                              {
+                                    if (j==0)
+                                    {
+                                          /* code */
+                                          for(n=0; n<6;n++)
+                                          {
+                                                datos[j].arp_mac[n]=data[22+n] ;
+                                          }
+                                          for(n=0; n<4;n++)
+                                          {
+                                                datos[j].arp_ip[n]=data[28+n] ;
+                                          }
+                                    }
+                                    else
+                                    {
+                                          int banderaIp=0;
+                                          int banderaMac=0;
+                                          int ipX, ipY;
+
+                                          for (ipX = 0; ipX < j; ipX++)
+                                          {
+                                                for (ipY = 0; ipY < 4; ipY++)
+                                                {
+                                                      if (data[28+ipY] == datos[ipX].arp_ip[ipY])
+                                                      {
+                                                            coincideip++;
+                                                      }
+                                                }
+                                                if (coincideip == 4)
+                                                {
+                                                      banderaIp =1;
+                                                      break;
+                                                }
+                                            
+                                                coincideip=0;
+                                          }
+                                          if (banderaIp != 0){
+                                                banderaMac = 1;
+                                          }
+
+                                          if (banderaMac == 1)
+                                          {
+                                                int macX, macY;
+                                                      for (macY = 0; macY  < 6; macY++)
+                                                      {
+                                                            if (data[22+macY] == datos[ipX].arp_mac[macY])
+                                                            {
+                                                                  coincideip++;
+                                                            }
+                                                      }
+                                                      if (coincidemac != 6){
+                                                            spoofarp = 1;
+                                                            v = macX;
+                                                      }
+                                                      coincidemac = 0;
+                                                j--;
+                                          }
+                                          else{
+                                                for(n=0; n<6;n++){
+                                                      datos[j].arp_mac[n]=data[22+n] ;
+                                                }
+                                                for(n=0; n<4;n++){
+                                                      datos[j].arp_ip[n]=data[28+n] ;
+                                                }     
+                                          }
+                                    }
+                                    j++;
+                              }
+                        }
+                            if ( (i % 16) == 0) {
+                           printf("\n"); // Start printing on the next after every 16 octets
+                            printf("%.2x ", data[i]); // Print each octet as hex (x), make sure there is always two characters (.2).   
+                        }
+                        if (spoofarp == 1){
+                              printf("\n\tDETECT: Who-has %i.%i.%i.%i, R1: %x:%x:%x:%x:%x:%x,  R2:  %x:%x:%x:%x:%x:%x, TS: %li . %li \n"
+                                    ,data[28],data[29],data[30],data[31]
+                                    ,datos[v].arp_mac[0], datos[v].arp_mac[1], datos[v].arp_mac[2], datos[v].arp_mac[3], datos[v].arp_mac[4], datos[v].arp_mac[5]
+                                    ,data[22],data[23],data[24],data[25],data[26], data[27]
+                                    ,header->ts.tv_sec, header->ts.tv_usec);
+                        }
+                        printf("\n\n");          // Add two lines between packets    
+
+
+
+                  
+
+                //*****************************************************************************************
+                  
+
+             }
+
+        
+        }
+        
+     }while((strcmp(opciones,"-i") != 0) && (strcmp(opciones,"-r") != 0);
 
 }
+
+
+
+
